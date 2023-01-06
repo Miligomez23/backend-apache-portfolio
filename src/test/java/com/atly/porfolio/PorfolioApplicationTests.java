98875c623cf0585881e654c35b46857989f15a49
Miligomez23 <mililgomez23@gmail.com>
2023-01-06 19:28:26 -0300

    deploy2.0

--- src/test/java/com/atly/porfolio/PorfolioApplicationTests.java
+++ src/test/java/com/atly/porfolio/PorfolioApplicationTests.java
@@ -3,7 +3,7 @@ package com.atly.porfolio;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = PorfolioApplicationTests.class)
 class PorfolioApplicationTests {
 
 	@Test
